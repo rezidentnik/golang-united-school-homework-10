c6281de75ea05687b75a291d0c515f722463fcc7
admin <admin@admin.net>
2022-06-27 17:31:53 +0300

    add hw solution

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -19,10 +20,16 @@ main function reads host/port from env just for an example, flavor it following 
 
 // Start /** Starts the web server listener on given host and port.
 func Start(host string, port int) {
-	router := mux.NewRouter()
+	r := mux.NewRouter()
+
+	r.HandleFunc("/name/{param}", nameHandler).Methods(http.MethodGet)
+	r.HandleFunc("/bad", badHandler).Methods(http.MethodGet)
+	r.HandleFunc("/data", dataHandler).Methods(http.MethodPost)
+	r.HandleFunc("/headers", headersHandler).Methods(http.MethodPost)
+	r.NotFoundHandler = http.HandlerFunc(notFoundHandler)
 
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
-	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), r); err != nil {
 		log.Fatal(err)
 	}
 }
@@ -35,4 +42,30 @@ func main() {
 		port = 8081
 	}
 	Start(host, port)
+}
+
+func nameHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(fmt.Sprintf("Hello, %s", mux.Vars(r)["param"])))
+}
+
+func badHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
+func dataHandler(w http.ResponseWriter, r *http.Request) {
+	body, _ := io.ReadAll(r.Body)
+	w.Write([]byte("I got message:\n"))
+	w.Write([]byte(string(body)))
+}
+
+func headersHandler(w http.ResponseWriter, r *http.Request) {
+	a, _ := strconv.Atoi(r.Header.Get("a"))
+	b, _ := strconv.Atoi(r.Header.Get("b"))
+	c := strconv.Itoa(a + b)
+
+	w.Header().Set("a+b", c)
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 }
